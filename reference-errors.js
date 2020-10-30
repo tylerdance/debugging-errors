a8deb404a0ba99f8a3c997058382b13efa07958d
Rome Bell <romebell5@gmail.com>
2020-10-30 08:19:36 -0700

    [reference errors] Attempt to add list:

--- reference-errors.js
+++ reference-errors.js
@@ -5,6 +5,15 @@
 console.log(foo.substring(1)) // ReferenceError: Cannot access 'foo' before initialization
 let foo = 'bar';
 
+// What's wrong here?
+function addNumbers() { 
+    let num1 = 2;
+    let num2 = 3;
+    return num1 + num2;
+  }
+  
+  console.log(num1); // ReferenceError num1 is not defined.
+
 /****************************************************************************
 Make 3-5 different examples of reference errors. 
 Look at the console to see what line the error is on to know what to do when 
